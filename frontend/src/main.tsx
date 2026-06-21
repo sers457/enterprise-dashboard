7c5327292c22a27afa4038294293012ab27d840a
sers457 <sers457@users.noreply.github.com>
2026-06-21 15:16:18 +0530

    Switch to HashRouter for GitHub Pages SPA

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
@@ -20,7 +20,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QueryClientProvider client={queryClient}>
         <ErrorBoundary><App /></ErrorBoundary>
         <Toaster
@@ -38,6 +38,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
           }}
         />
       </QueryClientProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
